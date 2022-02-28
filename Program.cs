988c7de6a78fe0c659d3ccaa2cef1f14cebeebda
Siddhant <santoshshrivastava39@gmail.com>
2022-02-28 16:31:41 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    /// <summary>
+    /// contains the main method.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
