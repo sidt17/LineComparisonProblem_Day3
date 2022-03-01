ba46d6fe2a66021a34c3e27d2d823f1e78d0338e
Siddhant <santoshshrivastava39@gmail.com>
2022-03-01 17:34:52 +0530

    Update the main method

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ namespace LineComparisonProblem
     class Program
     {
         /// <summary>
-        /// 
+        /// Calculated the line length.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            Cartesiansystem.Calculatinglength();
         }
     }
 }
